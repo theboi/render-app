a58fde5dd95f86323ea8c22f24b28956f0a8b3e6
Ryan The <ryan.the.2006@gmail.com>
2020-04-17 14:37:47 +0800

    Initial Commit

---
+++ Render/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Render
+//
+//  Created by Ryan The on 17/4/20.
+//  Copyright © 2020 Ryan The. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
